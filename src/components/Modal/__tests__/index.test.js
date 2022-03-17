d0658c5591dd902520ead40cd39aa98385f18163
joe <hudakjoe@msn.com>
2022-03-16 23:24:12 -0400

    gh-pages

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -2,3 +2,19 @@ import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Modal from 'Modal';
+
+afterEach(cleanup);
+
+const currentPhoto = {
+  name: 'Park bench',
+  category: 'landscape',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  index: 1,
+};
+
+describe('Modal component', () => {
+  it('renders', () => {
+    // baseline render component test
+  })
+    // snapshot test
